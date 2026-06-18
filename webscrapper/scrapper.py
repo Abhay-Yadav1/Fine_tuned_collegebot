b9838fc5347eeeb156204ae7b8331264705dc437
Abhay-Yadav1 <abhayadav1807@gmail.com>
2026-06-18 10:39:06 +0530

    Scrapped QNA pairs

--- webscrapper/scrapper.py
+++ webscrapper/scrapper.py
@@ -1,9 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 url = "https://rtionline.gov.in/faq.php"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-QnA={}
+
+QnA = {}
 container = soup.find('div', id='faq-From-div')
-print(type(container))
-print(container.find_all('li')[:5])
+
+for item in container.find_all('li'):
+    a_tag = item.find('a')
+    question = a_tag.get_text(strip=True)
+
+    answer_div = item.find('div', id='Answer')
+    answer = answer_div.get_text(strip=True)
+
+    QnA[question] = answer
+
+with open('output.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Key', 'Value'])  # Header
+    for key, value in QnA.items():
+        writer.writerow([key, value])
+
